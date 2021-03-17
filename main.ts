32c181441ae97c725cdb44caf24c79ce15f0ad14
ty89i <79651801+ty89i@users.noreply.github.com>
2021-03-17 16:11:56 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let naim = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 3 f f . . 
+    . . f d f f 3 5 5 3 f d f . . . 
+    . . . f f 3 3 3 3 3 f d f . . . 
+    . . . f 3 3 5 3 3 5 3 f f . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . . f f . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(naim, 100, 100)
+scene.setBackgroundImage(assets.image`idk`)
+tiles.setTilemap(tilemap`level1`)
